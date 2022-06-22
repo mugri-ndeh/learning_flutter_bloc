c466cf58099b04751e596fd571eb573487101c6a
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-22 11:28:58 +0100

    added bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learning_flutter_bloc/counter_app/cubit/counter_cubit.dart';
 import 'package:learning_flutter_bloc/counter_app/ui/counter_home.dart';
 
 void main() {
@@ -11,12 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const CounterHome(),
       ),
-      home: const CounterHome(),
     );
   }
 }
