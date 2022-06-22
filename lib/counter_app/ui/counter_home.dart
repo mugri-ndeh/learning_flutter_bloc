38350e8a052556c70ccb3bae895f9cc65559f030
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-22 09:55:19 +0100

    ui ok

--- lib/counter_app/ui/counter_home.dart
+++ lib/counter_app/ui/counter_home.dart
@@ -8,8 +8,50 @@ class CounterHome extends StatefulWidget {
 }
 
 class _CounterHomeState extends State<CounterHome> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.blueGrey.shade100,
+          ),
+          height: 200,
+          width: size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 40),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: const CircleAvatar(
+                      radius: 35,
+                      child: Icon(Icons.add, size: 50),
+                    ),
+                  ),
+                  const SizedBox(width: 40),
+                  InkWell(
+                    onTap: () {},
+                    child: const CircleAvatar(
+                      radius: 35,
+                      child: Icon(Icons.remove, size: 50),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
